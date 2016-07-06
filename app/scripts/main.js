7c917c82b8b267e64d4fe3302fe870107d582290
titay2 <tehetenamas@gmail.com>
2016-07-06 17:14:59 +0300

    adding the lists of project folder to the side bar menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,3 +26,21 @@ if (el) {
     document.querySelector("#create-app").style.display = "block";
   });
 }
+$(function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
